0d59f6af494cf228db9517de015d17567ff76cd3
Deniz Egemen Coşkun <denizegemencoskun@gmail.com>
2021-04-08 09:35:14 +0300

    DB.js :: add CREATE TABLE command example.

--- src/Repository/DB.js
+++ src/Repository/DB.js
@@ -3,6 +3,15 @@
 var mysql = require('mysql');
 
 //TODO: Create database and table during DevOps
+/*
+CREATE TABLE admins (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(32) NOT NULL,
+  password_hash VARCHAR(64) NOT NULL,
+  PRIMARY KEY (id)
+);
+*/
+
 var connection = mysql.createConnection({
   host: process.env.AUTH_MYSQL_SERVICE_SERVICE_HOST,
   user: "root",
